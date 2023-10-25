b5e16f139bf28f563f68d746c438a913467af0ba
VitordSmota <vitormota05@gmail.com>
2023-10-25 20:03:08 -0300

    criação das classes principais

---
+++ projeto/src/Model/Aeronave.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Aeronave {
+    protected String modelo;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+
+}
